61530f47fa836c411afe374f6da0580a2d2ad35d
Balaji-G-site <gundlapallibalaji@gmail.com>
2025-09-27 21:13:25 +0530

    roboshop

--- frontend.sh
+++ frontend.sh
@@ -32,33 +32,33 @@ VALIDATE () {
 }
 
 dnf module disable nginx -y &>>$LOG_FILE
-VALIDATE $? "disable default nginx"
+VALIDATE $? "Disabling Default Nginx"
 
 dnf module enable nginx:1.24 -y &>>$LOG_FILE
-VALIDATE $? "enabling nginx:1.24"
+VALIDATE $? "Enabling Nginx:1.24"
 
 dnf install nginx -y &>>$LOG_FILE
-VALIDATE $? "installing nginx"
+VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx &>>$LOG_FILE
-systemctl start nginx
-VALIDATE $? "starting nginx"
+systemctl enable nginx  &>>$LOG_FILE
+systemctl start nginx 
+VALIDATE $? "Starting Nginx"
 
-rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
-VALIDATE $? "removing default content"
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "Removing default content"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-VALIDATE $? "downloading frontend"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading frontend"
 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "unzipping frontend"
 
 rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
-VALIDATE $? "removing default content"
+VALIDATE $? "Remove default nginx conf"
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VALIDATE $? "copying nginx conf"
+VALIDATE $? "Copying nginx.conf"
 
 systemctl restart nginx 
 VALIDATE $? "Restarting nginx"
